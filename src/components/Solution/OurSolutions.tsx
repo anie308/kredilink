752ace61cb1c61ddc8541926c243eaf41ef6f9ca
Aniebiet Obo <aniebietabasiobo@gmail.com>
2025-01-30 23:38:01 +0100

    commit

--- src/components/Solution/OurSolutions.tsx
+++ src/components/Solution/OurSolutions.tsx
@@ -11,6 +11,7 @@ function OurSolutions() {
           </div>
           <div className="mt-[40px] grid gap-[40px] lg:grid-cols-2">
           <div className="bg-[#F2F7F6] rounded-[10px] lg:rounded-[20px] text-black p-[40px]">
+            <p className="text-[32px] lg:text-[64px] lading-[40px] lg:leading-[68px]">Treasury Management</p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore praesentium beatae ullam dolorem accusantium molestias possimus impedit est! Excepturi aliquid, illum eveniet magni veritatis ullam non est voluptas architecto sequi!
           </div>
           <div className="bg-[#F2F7F6] rounded-[10px] lg:rounded-[20px] text-black p-[40px]">
--- src/components/Support/SupportHero.tsx
+++ src/components/Support/SupportHero.tsx
@@ -2,11 +2,11 @@
 
 function SupportHero() {
   return (
-    <div className="flex flex-col items-center justify-center p-[40px_20px] lg:p-[40px]">
+    <div className="flex flex-col items-center justify-center p-[40px_20px] lg:p-[100px_40px]">
       <p className="text-[32px] lg:text-[64px] leading-[40px] lg:leading-[70px] text-center text-[#141414]">Ready To Redefine Your <br className="hidden lg:block" /> Financial Trading?</p>
       <p className="mt-[10px] text-center">At Kredilink, we don't just answer emails - we'll simplify your questions and concerns until  <br className="hidden lg:block" /> you are finally prepared to transform your business's financial operations.</p>
 
-      <button className="bg-[#1E3677] text-white p-[13px_30px] mt-[15px] rounded-[10px]">Contact Sales</button>
+      <button className="bg-[#1E3677] text-white p-[13px_30px] mt-[20px] rounded-[10px]">Contact Sales</button>
     </div>
   )
 }
